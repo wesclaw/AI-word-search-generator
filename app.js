f52d0d6a5b745996c800c0f6164657db86d93b71
wesclaw <108541397+wesclaw@users.noreply.github.com>
2024-07-21 18:33:57 +0200

    up

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
 //     messages: [
 //       {
 //         role: 'system',
-//         content: 'make me a word serch puzzle for the topic space. 5 rows with 5 columns of letters. only one word to find.'
+//         content: 'make me a word serch puzzle for the topic space. 10 rows with 10 columns of letters. only one word to find and its diagonal.'
 //       },
 //       {
 //         role: 'user',
@@ -25,33 +25,6 @@
 // }
 
 // main()
-
-
-require('dotenv').config()
-
-const PORT = process.env.PORT
-
-const express = require('express')
-const path = require('path')
-
-const OpenAI = require('openai').OpenAI;
-
-const app = express()
-
-const openai = new OpenAI({
-  apiKey: process.env.OPEN_AI_API_KEY,
-})
-
-app.get('/', (req, res)=>{
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
-
-
-app.listen(PORT, ()=>{
-  console.log('server is on port 3000')
-})
-
-
 
 
 
